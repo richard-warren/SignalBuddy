b754b664fa3373ef549e59132acba93e6a683fbd
rwarren2163 <richardwarren2163@gmail.com>
2019-09-11 16:45:06 -0400

    changed presets

--- SignalBuddy/config.h
+++ SignalBuddy/config.h
@@ -10,35 +10,51 @@ ledAmplitudeScaling = true;        // whether the LED amplitude should scale wit
 // preset 0
 presets[0].type = SINE;
 presets[0].duration = 1000;
-presets[0].amplitude = .1;
+presets[0].amplitude = .4;
 presets[0].frequency = 5;
 presets[0].rampUpTime = 100;
-presets[0].rampDownTime = 100;
+presets[0].rampDownTime = 400;
 presets[0].pulseDuration = 0;
 
 // preset 1
 presets[1].type = SINE;
-presets[1].duration = 5000;
-presets[1].amplitude = .5;
+presets[1].duration = 500;
+presets[1].amplitude = 1;
 presets[1].frequency = 20;
-presets[1].rampUpTime = 200;
+presets[1].rampUpTime = 0;
 presets[1].rampDownTime = 200;
 presets[1].pulseDuration = 0;
 
 // preset 2
 presets[2].type = STEP;
-presets[2].duration = 100;
-presets[2].amplitude = 1.0;
-presets[2].frequency = 0;
+presets[2].duration = 200;
+presets[2].amplitude = 0.25;
 presets[2].rampUpTime = 0;
-presets[2].rampDownTime = 200;
+presets[2].rampDownTime = 0;
 presets[2].pulseDuration = 0;
 
 // preset 3
-presets[3].type = PULSE;
-presets[3].duration = 2000;
-presets[3].amplitude = .5;
-presets[3].frequency = 5;
-presets[3].rampUpTime = 0;
-presets[3].rampDownTime = 0;
+presets[3].type = STEP;
+presets[3].duration = 800;
+presets[3].amplitude = .7;
+presets[3].rampUpTime = 150;
+presets[3].rampDownTime = 150;
 presets[3].pulseDuration = 50;
+
+// preset 3
+presets[4].type = PULSE;
+presets[4].duration = 1000;
+presets[4].amplitude = .85;
+presets[4].frequency = 10;
+presets[4].rampUpTime = 0;
+presets[4].rampDownTime = 0;
+presets[4].pulseDuration = 25;
+
+// preset 3
+presets[5].type = PULSE;
+presets[5].duration = 600;
+presets[5].amplitude = .55;
+presets[5].frequency = 15;
+presets[5].rampUpTime = 100;
+presets[5].rampDownTime = 0;
+presets[5].pulseDuration = 30;
